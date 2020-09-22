0a7b9203e96d606e3962f0b9b8f5414b0efc66cb
stelmakhivan <stelmakh.developer@gmail.com>
2020-09-22 20:51:49 +0300

    Connect to apollo-express-server

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,13 @@
 import express from 'express'
-import bodyParser from 'body-parser'
+import { ApolloServer } from 'apollo-server-express'
 
-import { listings } from './listings'
+import { typeDefs, resolvers } from './graphql'
 
 const app = express()
 const port = 9000
 
-app.use(bodyParser.json())
-
-app.get('/listings', (_req, res) => res.send(listings))
-
-app.post('/delete-listing', (req, res) => {
-  const id: string = req.body.id
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1))
-    }
-  }
-
-  return res.send('failed to delete listing')
-})
+const server = new ApolloServer({ typeDefs, resolvers })
+server.applyMiddleware({ app, path: '/api' })
 
 app.listen(port, () => {
   console.log(`[app]: Server is running on port  ${port}`)
