2fc1c976148233378da554b695d37e825370370c
stelmakhivan <stelmakh.developer@gmail.com>
2020-10-18 13:40:58 +0300

    fix: X-CSRF-TOKEN fixed typo

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -5,7 +5,7 @@ export const authorize = async (
   db: Database,
   req: Request
 ): Promise<User | null> => {
-  const token = req.get('X_CSRF-TOKEN')
+  const token = req.get('X-CSRF-TOKEN')
   const viewer = await db.users.findOne({
     _id: req.signedCookies.viewer,
     token,
