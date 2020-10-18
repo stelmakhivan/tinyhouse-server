48b866a439a16bd711e404284ea83a15370a7623
stelmakhivan <stelmakh.developer@gmail.com>
2020-10-18 14:02:41 +0300

    feat: updated type defs

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,6 @@
 import merge from 'lodash.merge'
 
 import { viewerResolvers } from './Viewers'
+import { userResolvers } from './User'
 
-export const resolvers = merge(viewerResolvers)
+export const resolvers = merge(viewerResolvers, userResolvers)
